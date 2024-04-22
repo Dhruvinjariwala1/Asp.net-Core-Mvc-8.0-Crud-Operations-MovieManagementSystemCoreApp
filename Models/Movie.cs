a68b36c89b977e9aed22804bd21c246f9fddbe54
Dhruvin Jariwala <dhruvinjariwala422002@gmail.com>
2024-04-22 22:38:22 +0530

    Add project files.

---
+++ Models/Movie.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MovieManagementSystemCoreApp.Models;
+
+public partial class Movie
+{
+    public int Movieid { get; set; }
+
+    public string Moviename { get; set; } = null!;
+
+    public string DirectorName { get; set; } = null!;
+
+    public string ProducerName { get; set; } = null!;
+
+    public string ActorName { get; set; } = null!;
+
+    public string ActressName { get; set; } = null!;
+
+    public string MovieVerdict { get; set; } = null!;
+
+    public string MovieBudget { get; set; } = null!;
+
+    public string MovieRating { get; set; } = null!;
+
+    public string MovieReleaseYear { get; set; } = null!;
+}
